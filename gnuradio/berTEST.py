9a3bda013249274aa63ce2a6c2c5c7c8d8551872
Live session user <ubuntu@ubuntu.(none)>
2014-08-01 11:30:40 -0300

    BER block and its test created in gnuradio. Also, gnuradio directory created.

---
+++ gnuradio/berTEST.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: BER Test
+# Author: Daniel Franch
+# Description: Tests the BER block created
+# Generated: Fri Aug  1 11:28:43 2014
+##################################################
+
+execfile("/home/ubuntu/.grc_gnuradio/bitErrorRate.py")
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.wxgui import numbersink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import numpy
+import wx
+
+class berTEST(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="BER Test")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.wxgui_numbersink2_0 = numbersink2.number_sink_f(
+        	self.GetWin(),
+        	unit="%",
+        	minval=-100,
+        	maxval=100,
+        	factor=1.0,
+        	decimal_places=10,
+        	ref_level=0,
+        	sample_rate=samp_rate,
+        	number_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	label="BER",
+        	peak_hold=False,
+        	show_gauge=True,
+        )
+        self.Add(self.wxgui_numbersink2_0.win)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((100, ))
+        self.bitErrorRate_0 = bitErrorRate()
+        self.analog_random_source_x_0_0 = blocks.vector_source_b(map(int, numpy.random.randint(0, 2, 16)), True)
+        self.analog_random_source_x_0 = blocks.vector_source_b(map(int, numpy.random.randint(0, 2, 16)), True)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.wxgui_numbersink2_0, 0))
+        self.connect((self.analog_random_source_x_0_0, 0), (self.bitErrorRate_0, 1))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.bitErrorRate_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.analog_random_source_x_0, 0), (self.bitErrorRate_0, 0))
+
+
+# QT sink close method reimplementation
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = berTEST()
+    tb.Start(True)
+    tb.Wait()
+
