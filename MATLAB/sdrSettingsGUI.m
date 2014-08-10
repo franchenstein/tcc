927f5ec3a4eaf8a3d88c403da2aab7f9c0d11e18
Franch <dfranch6@gmail.com>
2014-08-10 18:01:43 -0300

    GUI finished: all configurable values can now be saved in the sdrSettings file

--- MATLAB/sdrSettingsGUI.m
+++ MATLAB/sdrSettingsGUI.m
@@ -22,7 +22,7 @@ function varargout = sdrSettingsGUI(varargin)
 
 % Edit the above text to modify the response to help sdrSettingsGUI
 
-% Last Modified by GUIDE v2.5 06-Aug-2014 15:45:34
+% Last Modified by GUIDE v2.5 10-Aug-2014 17:54:13
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -240,6 +240,37 @@ function pushbutton1_Callback(hObject, eventdata, handles)
         'r', r);
     global fc;
     global fs;
+    global rxFc;
+    global rxFs;
+    global rxFilterType;
+    global rxSps;
+    global rxT;
+    global rxNt;
+    global rxR;
+    rxF = struct(...
+            'type', rxFilterType,...
+            'sps', rxSps,...
+            'T', rxT,...
+            'nt', rxNt,...
+            'r', rxR);
+    global rxModSchm;
+    global rxM;
+    global rxCodingAlgorithm;
+    global rxK;
+    global rxN;
+    rxCodingParams = struct('n', rxN, 'k', rxK);
+    global clStep;
+    global elStep;
+    global agcStep;
+    global rxMLength;
+    global rxOversample;
+    global sinkFile;
+    global enableAGC;
+    global enableEL;
+    global enableCL;
+    global enableCorr;
+    global enableCoding;
+    global sinkFile;
     save('sdrSettings.mat', 'nGain', '-append');
     save('sdrSettings.mat', 'timingOffset', '-append');
     save('sdrSettings.mat', 'fp', '-append');
@@ -257,6 +288,24 @@ function pushbutton1_Callback(hObject, eventdata, handles)
     save('sdrSettings.mat', 'f', '-append');
     save('sdrSettings.mat', 'fc', '-append');
     save('sdrSettings.mat', 'fs', '-append');
+    save('sdrSettings.mat', 'rxFs', '-append');
+    save('sdrSettings.mat', 'rxFc', '-append');
+    save('sdrSettings.mat', 'rxF', '-append');
+    save('sdrSettings.mat', 'rxModSchm', '-append');
+    save('sdrSettings.mat', 'rxM', '-append');
+    save('sdrSettings.mat', 'rxCodingAlgorithm', '-append');
+    save('sdrSettings.mat', 'rxCodingParams', '-append');
+    save('sdrSettings.mat', 'clStep', '-append');
+    save('sdrSettings.mat', 'elStep', '-append');
+    save('sdrSettings.mat', 'agcStep', '-append');
+    save('sdrSettings.mat', 'rxMLength', '-append');
+    save('sdrSettings.mat', 'rxOversample', '-append');
+    save('sdrSettings.mat', 'sinkFile', '-append');   
+    save('sdrSettings.mat', 'enableAGC', '-append'); 
+    save('sdrSettings.mat', 'enableEL', '-append');
+    save('sdrSettings.mat', 'enableCL', '-append');
+    save('sdrSettings.mat', 'enableCorr', '-append');
+    save('sdrSettings.mat', 'enableCoding', '-append');
 
 
 % --- If Enable == 'on', executes on mouse press in 5 pixel border.
@@ -757,6 +806,8 @@ function edit18_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit18 as text
 %        str2double(get(hObject,'String')) returns contents of edit18 as a double
+    global rxFc;
+    rxFc = str2double(get(hObject, 'String'));
 
 
 % --- Executes during object creation, after setting all properties.
@@ -770,7 +821,8 @@ function edit18_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-
+    global rxFc;
+    rxFc = 433e6;
 
 
 function edit19_Callback(hObject, eventdata, handles)
@@ -780,6 +832,8 @@ function edit19_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit19 as text
 %        str2double(get(hObject,'String')) returns contents of edit19 as a double
+    global rxFs;
+    rxFs = str2double(get(hObject, 'String'));
 
 
 % --- Executes during object creation, after setting all properties.
@@ -793,6 +847,8 @@ function edit19_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global rxFs;
+    rxFs = 1732e6;
 
 
 
@@ -803,6 +859,8 @@ function edit20_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit20 as text
 %        str2double(get(hObject,'String')) returns contents of edit20 as a double
+    global clStep;
+    clStep = str2double(get(hObject,'String'));
 
 
 % --- Executes during object creation, after setting all properties.
@@ -816,6 +874,8 @@ function edit20_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global clStep;
+    clStep = 10;
 
 
 
@@ -826,6 +886,9 @@ function edit21_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit21 as text
 %        str2double(get(hObject,'String')) returns contents of edit21 as a double
+    global sinkFile;
+    sinkFile = get(hObject, 'String');
+    
 
 
 % --- Executes during object creation, after setting all properties.
@@ -839,6 +902,8 @@ function edit21_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global sinkFile;
+    sinkFile = './rx_msg.txt';
 
 
 % --- Executes on selection change in popupmenu6.
@@ -849,6 +914,10 @@ function popupmenu6_Callback(hObject, eventdata, handles)
 
 % Hints: contents = cellstr(get(hObject,'String')) returns popupmenu6 contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from popupmenu6
+    global rxModSchm;
+    items = get(hObject, 'String');
+    index = get(hObject, 'Value');
+    rxModSchm = items{index};
 
 
 % --- Executes during object creation, after setting all properties.
@@ -862,6 +931,8 @@ function popupmenu6_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global rxModSchm;
+    rxModSchm = 'PSK';
 
 
 
@@ -872,6 +943,8 @@ function edit22_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit22 as text
 %        str2double(get(hObject,'String')) returns contents of edit22 as a double
+    global rxOversample;
+    rxOversample = str2double(get(hObject, 'String'));
 
 
 % --- Executes during object creation, after setting all properties.
@@ -885,6 +958,8 @@ function edit22_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global rxOversample;
+    rxOversample = 10;
 
 
 % --- Executes on selection change in popupmenu7.
@@ -895,6 +970,10 @@ function popupmenu7_Callback(hObject, eventdata, handles)
 
 % Hints: contents = cellstr(get(hObject,'String')) returns popupmenu7 contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from popupmenu7
+    global rxM;
+    items = get(hObject, 'String');
+    index = get(hObject, 'Value');
+    rxM = str2double(items{index});
 
 
 % --- Executes during object creation, after setting all properties.
@@ -908,6 +987,8 @@ function popupmenu7_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global rxM;
+    rxM = 2;
 
 
 % --- Executes on selection change in popupmenu9.
@@ -918,6 +999,10 @@ function popupmenu9_Callback(hObject, eventdata, handles)
 
 % Hints: contents = cellstr(get(hObject,'String')) returns popupmenu9 contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from popupmenu9
+    global rxCodingAlgorithm;
+    items = get(hObject, 'String');
+    index = get(hObject, 'Value');
+    rxCodingAlgorithm = items{index};
 
 
 % --- Executes during object creation, after setting all properties.
@@ -931,6 +1016,8 @@ function popupmenu9_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global rxCodingAlgorithm;
+    rxCodingAlgorithm = 'BCH';
 
 
 
@@ -941,6 +1028,8 @@ function edit27_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit27 as text
 %        str2double(get(hObject,'String')) returns contents of edit27 as a double
+    global rxK;
+    rxK = str2double(get(hObject, 'String'));
 
 
 % --- Executes during object creation, after setting all properties.
@@ -954,7 +1043,8 @@ function edit27_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-
+    global rxK;
+    rxK = 199;
 
 
 function edit28_Callback(hObject, eventdata, handles)
@@ -964,6 +1054,8 @@ function edit28_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit28 as text
 %        str2double(get(hObject,'String')) returns contents of edit28 as a double
+    global rxN;
+    rxN = str2double(get(hObject, 'String'));
 
 
 % --- Executes during object creation, after setting all properties.
@@ -977,6 +1069,8 @@ function edit28_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global rxN;
+    rxN = 255;
 
 
 % --- Executes on selection change in popupmenu8.
@@ -987,6 +1081,10 @@ function popupmenu8_Callback(hObject, eventdata, handles)
 
 % Hints: contents = cellstr(get(hObject,'String')) returns popupmenu8 contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from popupmenu8
+    global rxFilterType;
+    items = get(hObject,'String');
+    index = get(hObject, 'Value');
+    rxFilterType = items{index};
 
 
 % --- Executes during object creation, after setting all properties.
@@ -1000,7 +1098,8 @@ function popupmenu8_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-
+    global rxFilterType;
+    rxFilterType = 'Root-Raised-Cosine';
 
 
 function edit23_Callback(hObject, eventdata, handles)
@@ -1010,6 +1109,8 @@ function edit23_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit23 as text
 %        str2double(get(hObject,'String')) returns contents of edit23 as a double
+    global rxSps;
+    rxSps = str2double(get(hObject, 'String'));
 
 
 % --- Executes during object creation, after setting all properties.
@@ -1023,6 +1124,8 @@ function edit23_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global rxSps;
+    rxSps = 10;
 
 
 
@@ -1033,6 +1136,8 @@ function edit24_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit24 as text
 %        str2double(get(hObject,'String')) returns contents of edit24 as a double
+    global rxT;
+    rxT = str2double(get(hObject, 'String'));
 
 
 % --- Executes during object creation, after setting all properties.
@@ -1046,6 +1151,8 @@ function edit24_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global rxT;
+    rxT = 1;
 
 
 
@@ -1056,6 +1163,9 @@ function edit25_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit25 as text
 %        str2double(get(hObject,'String')) returns contents of edit25 as a double
+    global rxNt;
+    length = str2double(get(hObject, 'String'));
+    rxNt = (length - 1)/2;
 
 
 % --- Executes during object creation, after setting all properties.
@@ -1069,6 +1179,8 @@ function edit25_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global rxNt;
+    rxNt = 5;
 
 
 
@@ -1079,6 +1191,8 @@ function edit26_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit26 as text
 %        str2double(get(hObject,'String')) returns contents of edit26 as a double
+    global rxR;
+    rxR = str2double(get(hObject, 'String'));
 
 
 % --- Executes during object creation, after setting all properties.
@@ -1092,6 +1206,8 @@ function edit26_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global rxR;
+    rxR = 0.5;
 
 
 
@@ -1102,7 +1218,9 @@ function edit29_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit29 as text
 %        str2double(get(hObject,'String')) returns contents of edit29 as a double
-
+    global agcStep;
+    agcStep = str2double(get(hObject, 'String'));
+    
 
 % --- Executes during object creation, after setting all properties.
 function edit29_CreateFcn(hObject, eventdata, handles)
@@ -1115,6 +1233,8 @@ function edit29_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global agcStep;
+    agcStep = 0.05;
 
 
 
@@ -1125,6 +1245,8 @@ function edit30_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit30 as text
 %        str2double(get(hObject,'String')) returns contents of edit30 as a double
+    global rxMLength;
+    rxMLength = str2double(get(hObject, 'String'));
 
 
 % --- Executes during object creation, after setting all properties.
@@ -1138,6 +1260,8 @@ function edit30_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global rxMLength;
+    rxMLength = 10000;
 
 
 
@@ -1148,6 +1272,8 @@ function edit31_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit31 as text
 %        str2double(get(hObject,'String')) returns contents of edit31 as a double
+    global elStep;
+    elStep = str2double(get(hObject, 'String'));
 
 
 % --- Executes during object creation, after setting all properties.
@@ -1161,6 +1287,8 @@ function edit31_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global elStep;
+    elStep = 0.01;
 
 
 % --- Executes on selection change in popupmenu10.
@@ -1171,6 +1299,10 @@ function popupmenu10_Callback(hObject, eventdata, handles)
 
 % Hints: contents = cellstr(get(hObject,'String')) returns popupmenu10 contents as cell array
 %        contents{get(hObject,'Value')} returns selected item from popupmenu10
+    global matchfilt;
+    items = get(hObject, 'String');
+    index = get(hObject, 'Value');
+    matchfilt = items{index};
 
 
 % --- Executes during object creation, after setting all properties.
@@ -1184,6 +1316,8 @@ function popupmenu10_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+    global matchfilt;
+    matchfilt = 'Matched';
 
 
 % --- Executes on button press in checkbox1.
@@ -1193,6 +1327,8 @@ function checkbox1_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of checkbox1
+    global enableAGC;
+    enableAGC = get(hObject, 'Value');
 
 
 % --- Executes on button press in checkbox2.
@@ -1202,6 +1338,8 @@ function checkbox2_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of checkbox2
+    global enableCL;
+    enableCL = get(hObject, 'Value');
 
 
 % --- Executes on button press in checkbox3.
@@ -1211,6 +1349,8 @@ function checkbox3_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of checkbox3
+    global enableEL;
+    enableEL = get(hObject, 'Value');
 
 
 % --- Executes on button press in checkbox4.
@@ -1220,7 +1360,8 @@ function checkbox4_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of checkbox4
-
+    global enableCorr;
+    enableCorr = get(hObject, 'Value');
 
 % --- Executes on button press in checkbox5.
 function checkbox5_Callback(hObject, eventdata, handles)
@@ -1229,3 +1370,50 @@ function checkbox5_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of checkbox5
+    global enableCoding;
+    enableCoding = get(hObject, 'Value');
+
+
+% --- Executes during object creation, after setting all properties.
+function checkbox1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to checkbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+    global enableAGC;
+    enableAGC = 1; 
+
+
+% --- Executes during object creation, after setting all properties.
+function checkbox2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to checkbox2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+    global enableCL;
+    enableCL = 1;
+
+
+% --- Executes during object creation, after setting all properties.
+function checkbox3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to checkbox3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+    global enableEL;
+    enableEL = 1;
+
+
+% --- Executes during object creation, after setting all properties.
+function checkbox4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to checkbox4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+    global enableCorr;
+    enableCorr = 1;
+
+
+% --- Executes during object creation, after setting all properties.
+function checkbox5_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to checkbox5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+    global enableCoding;
+    enableCoding = 1;
